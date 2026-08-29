269bc5696e6ef7857c1141222ddfacdbe1b584e4
Bhaskarmondal8 <bhaskar.mondal.co@gmail.com>
2026-08-29 18:33:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
 
-const app = express();
-
-app.use(cors());
-app.use(express.static(__dirname));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
